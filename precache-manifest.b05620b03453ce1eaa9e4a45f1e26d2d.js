8cb93bbcb25824d497d246d12a75925a34d4f816
Foysalmeazi <abdullah15-7683@diu.edu.bd>
2026-05-11 11:44:10 +0930

    Updates

---
+++ precache-manifest.b05620b03453ce1eaa9e4a45f1e26d2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c02058927e56a753b2fdbad95a227a",
+    "url": "/Foysalmeazi/ReactEcom/index.html"
+  },
+  {
+    "revision": "7278effbd407b0a6aaf7",
+    "url": "/Foysalmeazi/ReactEcom/static/css/2.63da010e.chunk.css"
+  },
+  {
+    "revision": "c39d4832c08c713933d7",
+    "url": "/Foysalmeazi/ReactEcom/static/css/main.1c2aec14.chunk.css"
+  },
+  {
+    "revision": "7278effbd407b0a6aaf7",
+    "url": "/Foysalmeazi/ReactEcom/static/js/2.1ccfc7cd.chunk.js"
+  },
+  {
+    "revision": "c39d4832c08c713933d7",
+    "url": "/Foysalmeazi/ReactEcom/static/js/main.1c5fddb5.chunk.js"
+  },
+  {
+    "revision": "f0db43d1fcdd40fcdaae",
+    "url": "/Foysalmeazi/ReactEcom/static/js/runtime-main.9574562f.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/Foysalmeazi/ReactEcom/static/media/logo.9f7e506d.svg"
+  }
+]);
